525b022a47d9ec0a752ae399a5b1017de16894a9
Akash Gogoi <gogoiakash09@gmail.com>
2023-07-01 22:43:54 +0530

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function Hero() {
+  return (
+    <main className="bg-primary flex justify-center items-center" >
+       <div className="w-[1280px] text-center circularstd-font">
+            <div className="">
+                <p>Timely time tracking software</p>
+                <div className="font-bold text-[112px] tracking-[-3.2px] leading-[100.8px] text-white">
+                    <h1>The world&#39;s best <br/>
+                    <span className="text-yellow" >AI-powered</span> automatic time tracker</h1>
+                </div>
+            </div>
+       </div>
+    </main>
+  )
+}
