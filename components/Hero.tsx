c4c5d4668ee313e4aa12918b9f9b3738a7fa2eeb
Akash Gogoi <gogoiakash09@gmail.com>
2023-07-02 21:34:32 +0530

    hero section update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,33 @@
 "use client"
+import Image from 'next/image';
+import Arrow from "../public/Arrow"
+
+
 
 export default function Hero() {
   return (
     <main className="bg-primary flex justify-center items-center" >
-       <div className="w-[1280px] text-center circularstd-font">
-            <div className="">
-                <p>Timely time tracking software</p>
-                <div className="font-bold text-[112px] tracking-[-3.2px] leading-[100.8px] text-white">
-                    <h1>The world&#39;s best <br/>
-                    <span className="text-yellow" >AI-powered</span> automatic time tracker</h1>
-                </div>
+      <div className="w-[1280px] text-center circularstd-font">
+        
+          <p className="text-[20px] font-bold tracking-[-0.32px] text-white "> <span className="text-yellow">Timely</span> time tracking software</p>
+          <div className="font-bold text-[112px] tracking-[-3.2px] leading-[100.8px] text-white mb-[24px]">
+            <h1>The world&#39;s best <br />
+              <span className="text-yellow" >AI-powered</span> automatic time tracker
+            </h1>
+          </div>
+          <div className="mb-[48px]" >
+            <p className="text-[24px] text-white" > <span className="text-yellow">Timely</span> automates company time tracking, so you and your team can <br /> <span className="text-yellow">focus on the work that matters.</span></p>
+          </div>
+
+          
+            <div className='flex justify-center ' >
+              <button className='flex text-black h-[52px] w-[160px]    rounded-2xl  justify-center items-center  bg-white mb-[96px] ' >Get Started
+                <div className='' ><Arrow /></div>
+              </button>
             </div>
-       </div>
+          
+        
+      </div>
     </main>
   )
 }
---
+++ public/Arrow.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Arrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25px"
+      height="100%"
+      viewBox="0 0 28 16"
+      fill="none"
+      preserveAspectRatio="xMidYMid meet"
+      aria-hidden="true"
+      role="img"
+      className="text-primary ml-2 "
+    >
+      <path
+        d="M2 8H25.5M25.5 8L19.5 2M25.5 8L19.5 14"
+        stroke="currentColor"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
