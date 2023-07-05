69b93aa86452972ef1a23a6f178195dcbd434e64
Akash Gogoi <gogoiakash09@gmail.com>
2023-07-05 12:51:06 +0530

    add pros section

--- components/Pros.tsx
+++ components/Pros.tsx
@@ -3,9 +3,23 @@ import Wire from "../public/Wire"
 
 export default function Pros() {
   return (
-    <main className='pros-section' >
-        <div className='absolute right-[835px] ' >
-            <Wire />
+    <main className='pros-section ' >
+        <div className=' left-[250px] absolute  ' >
+            
+            <div className='absolute' >
+              <div className=' w-[1280px] h-[1830px] border-2 mx-auto ' >
+
+                <div>
+                  
+                </div>
+                
+              </div>
+            </div>
+
+            <div className='absolute ' >
+              <Wire />
+            </div>
+
         </div>
     </main>
   )
