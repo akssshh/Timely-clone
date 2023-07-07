afcfd4f1af9380370f7ed6647cfc5887adf8af2e
Akash Gogoi <gogoiakash09@gmail.com>
2023-07-07 13:39:18 +0530

    update mega menu

--- components/MegaMenu1.tsx
+++ components/MegaMenu1.tsx
@@ -4,9 +4,9 @@ import TimeDD from "../public/time-dd.svg"
 export default function MegaMenu1() {
   return (
     <main className='max-w-[1920px] h-[608px] px-[96px]' >
-        <div className='max-w-[1728px]  ' >
+        <div className='max-w-[1728px] h-[608px]  ' >
             <div className='max-w-[1280px] mx-auto   flex  ' >
-                <div className='max-w-[902px] h-[526px] py-[40px]   ' >
+                <div className='max-w-[902px] h-[608px] py-[40px]   ' >
                     <div className='w-[902px] h-[152px] '>
                         <div className='w-[902px] h-[30px] circularstd-font text-[20px] text-bold  leading-[30px] mb-[16px] '>Features</div>
                         <div className='w-[902px] h-[106px] circularstd-font flex justify-between ' >
@@ -164,7 +164,38 @@ export default function MegaMenu1() {
 
 
 
-                    <div className="" ></div>
+                    <div className=" w-[902px] h-[112px] mt-[32px] " >
+                        <div className='w-[902px] h-[30px] circularstd-font text-[20px] text-bold  leading-[30px] mb-[16px] '>Our apps</div>
+                        <div className='w-[902px] h-[66px] circularstd-font flex gap-x-6' >
+
+                            <div className='w-[156px] h-[66px]' >
+                                <div className='w-[156px] py-[6px] h-[66px] flex flex-col justify-between '>
+                                    
+                                    <div className="flex flex-row w-[156px] h-[24px] justify-between  " >
+                                        <div className="h-[24px]" ><Image src={TimeDD} alt="clock-logo" /></div>
+                                        <div className=" w-[122px] circularstd-font text-[16px] leading-[24px] h-[24px] " >Memory tracker</div>
+                                    </div>
+
+                                    <p className="text-[13px]" >Let your work track itself</p>
+                                </div>
+                            </div>
+
+                            <div className='w-[156px] h-[66px]' >
+                                <div className='w-[156px] py-[6px] h-[66px] flex flex-col justify-between '>
+                                    
+                                    <div className="flex flex-row w-[156px] h-[24px] justify-between  " >
+                                        <div className="h-[24px]" ><Image src={TimeDD} alt="clock-logo" /></div>
+                                        <div className=" w-[122px] circularstd-font text-[16px] leading-[24px] h-[24px] " >Memory tracker</div>
+                                    </div>
+
+                                    <p className="text-[13px]" >Let your work track itself</p>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
 
 
 
