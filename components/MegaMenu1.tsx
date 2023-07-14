28c610b5d14479271cd7fb1652a1911fa391fb60
Akash Gogoi <gogoiakash09@gmail.com>
2023-07-14 12:45:01 +0530

    update dropdown

--- components/MegaMenu1.tsx
+++ components/MegaMenu1.tsx
@@ -7,7 +7,7 @@ export default function MegaMenu1() {
         <main className="bg-yellow">
 
             <div id="mega-menu-full-cta-dropdown" className="mt-1  max-w-[1280px] mx-auto ">
-                <div className="gap-x-4 grid grid-cols-2 grid-template py-5 mx-auto text-sm flex flex-col md:flex-row">
+                <div className="gap-x-4 md:grid grid-cols-2  grid-template py-5 grid-auto-cols-1fr ">
                     
                         <ul className="grid  gap-x-8 grid-cols-auto-fit grid-cols-minmax-10 " aria-labelledby="mega-menu-full-cta-button">
                             <li>
@@ -53,15 +53,11 @@ export default function MegaMenu1() {
                         </ul>
                     
 
-                    <div className="mt-4 md:mt-0">
-                        <h2 className="mb-2 font-semibold text-gray-900 dark:text-white">Our brands</h2>
-                        <p className="mb-2 text-gray-500 dark:text-gray-400">At Flowbite, we have a portfolio of brands that cater to a variety of preferences.</p>
-                        <a href="#" className="inline-flex items-center text-sm font-medium">
-                            Explore our brands
-                            <span className="sr-only">Explore our brands </span>
-                            <svg className="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
+                    <div className="mt-4 md:mt-0  ">
+                        <h2 className="mb-2 circularstd-font text-[20px] text-bold  leading-[30px] text-black">Integrations</h2>
+                        <a href="#" className="flex items-center">
+                            <div className=" w-[32px] h-[32px] " > <Image src={Asana} alt="asana-logo" /> </div>
+                            <div className="  h-[28px] text-[16px] circularstd-font leading-[24px]" >Asana</div>
                         </a>
                     </div>
 
